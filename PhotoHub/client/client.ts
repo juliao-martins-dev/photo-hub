b334079e042761741c455c8f2be2e5a3d0350a1e
Julião Martins <indianmobiledeveloper022@gmail.com>
2026-03-25 20:18:54 +0900

    feat: configure axios client with auth token interceptor

--- PhotoHub/client/client.ts
+++ PhotoHub/client/client.ts
@@ -1,27 +1,25 @@
 import axios from "axios";
-import * as SecureStore from 'expo-secure-store';
-import { router, useRouter } from "expo-router";
-
+import * as SecureStore from "expo-secure-store";
 
 const client = axios.create({
-    baseURL: process.env.EXPO_PUBLIC_BASE_URL,
-    timeout: 10000,
+  baseURL: process.env.EXPO_PUBLIC_BASE_URL,
+  timeout: 10000,
 });
 
 client.interceptors.request.use(async (config) => {
-    const token = await SecureStore.getItemAsync("token_access");
-    if (token) {
-        config.headers['Authorization'] = `Bearer ${token}`;
-    } 
-    return config;
+  const token = await SecureStore.getItemAsync("token_access");
+  if (token) {
+    config.headers["Authorization"] = `Bearer ${token}`;
+  }
+  return config;
 });
 
-client.interceptors.response.use(response => response, async error => {
-    const originalRequest = error.config;
-    if (error.response?.status === 401) {
-        router.replace("/");
-    }
-    return Promise.reject(error);
-});
+// client.interceptors.response.use(response => response, async error => {
+//     const originalRequest = error.config;
+//     if (error.response?.status === 401) {
+//         router.replace("/");
+//     }
+//     return Promise.reject(error);
+// });
 
 export default client;
