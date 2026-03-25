e1ade1edcb560a45ed5ce7a58b5feec414541c04
Julião Martins <indianmobiledeveloper022@gmail.com>
2026-03-25 20:19:22 +0900

    feat: add JWT token verify endpoint

--- apis/photos/urls.py
+++ apis/photos/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from django.urls import path
 from .views import UserViewSet, PhotoViewSet
 
@@ -10,6 +10,7 @@ router.register(r'photos', PhotoViewSet, basename='photo')
 
 
 urlpatterns = [
-    path('users/login/',         TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('users/token/refresh/', TokenRefreshView.as_view(),   name='token_refresh'),
+    path('users/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ] + router.urls
