23bcd0d1b435a69ffcde998de1ebb877830abdeb
Julião Martins <indianmobiledeveloper022@gmail.com>
2026-03-25 10:24:41 +0900

    chore(config): configure static and media file serving

--- apis/config/settings.py
+++ apis/config/settings.py
@@ -120,10 +120,16 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Media files (user uploaded photos)
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 AUTH_USER_MODEL = 'photos.User'
